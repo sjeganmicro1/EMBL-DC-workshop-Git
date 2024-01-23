b2942a053baac2101faee96bf65ae5c5a8f80bb9
Jegan Sekar <jegansekar@sjegan.local>
2024-01-23 20:22:13 +0530

    added script

--- scripts/day2.R
+++ scripts/day2.R
@@ -19,4 +19,36 @@ weight_kg <- 57
 
 weight_1b <- 2.2*weight_kg
 weight_1b
+#The wiegt has been updated 50 to 100
+
 wieght_kg <- 100
+
+mass = 47.5
+age = 122
+mass = mass * 2.0
+age = age - 20
+mass_index = mass/age
+
+sqrt
+
+sqrt(9)
+
+weight_kg = sqrt(9)
+
+#What is round ()
+
+round = 5*2.5
+round
+round = round
+round
+round (3.14512)
+round (3.14159, 2)
+age = 210/2.2
+age
+roundoff = round (digits 2, age)
+roundoff
+my_function = function (x) {
+  x = x+2
+}
+my_results = my_functions
+}
