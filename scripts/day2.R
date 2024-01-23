05c049aea655577b78bf032396eb98e897a4398c
Jegan Sekar <jegansekar@sjegan.local>
2024-01-23 19:31:44 +0530

    Day 2 first R script

---
+++ scripts/day2.R
@@ -0,0 +1,22 @@
+3+4
+3 + 4 
+3 + 4
+weight_kg <- 55
+
+mickey <- 27
+x2 <- 3
+
+
+my_weight_kg <- 50
+(my_weight_kg <- 50)
+(my_weight <- 50)
+my_weight
+my_weight
+
+2.2 * my_weight_kg
+
+weight_kg <- 57
+
+weight_1b <- 2.2*weight_kg
+weight_1b
+wieght_kg <- 100
